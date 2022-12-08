16334fa0b96ec17d6a802183ffb2b687e5b1e110
xu756 <756334744@qq.com>
2022-12-08 20:47:39 +0800

    Replace the module Use gin

--- main.go
+++ main.go
@@ -1,32 +1,16 @@
 package main
 
 import (
-	"github.com/labstack/echo/v4"
-	"github.com/labstack/echo/v4/middleware"
-	"net/http"
-	"time"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	e := echo.New()
-
-	// Middleware
-	e.Use(middleware.Logger())
-	e.Use(middleware.Recover())
-	r := e.Group("/api/v1/")
-	// Routes
-	r.GET("", hello).Name = "hello"
-
-	// Start server
-	s := &http.Server{
-		Addr:         ":8888",
-		ReadTimeout:  20 * time.Minute,
-		WriteTimeout: 20 * time.Minute,
-	}
-	e.Logger.Fatal(e.StartServer(s))
-}
-
-// Handler
-func hello(c echo.Context) error {
-	return c.String(200, "Hello, World!")
+	router := gin.Default()
+	r := router.Group("/api/v1/")
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	router.Run()
 }
