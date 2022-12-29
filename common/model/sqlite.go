aff012f1835c6cc03b0f38b79049cfdb0b88a39b
xu756 <756334744@qq.com>
2022-12-29 23:45:19 +0800

    sqlite

---
+++ common/model/sqlite.go
@@ -0,0 +1,53 @@
+package model
+
+import (
+	"fmt"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+	"log"
+	"time"
+)
+
+var SqliteDB *gorm.DB
+
+func InitsqLite() {
+	db, err := gorm.Open(sqlite.Open("logs.db"), &gorm.Config{})
+	if err != nil {
+		fmt.Println("sqlite数据库连接失败")
+	}
+	fmt.Println("sqlite数据库连接成功")
+	sqlDB, _ := db.DB()
+	sqlDB.SetMaxIdleConns(10)
+	sqlDB.SetMaxOpenConns(100)
+	sqlDB.SetConnMaxLifetime(time.Hour)
+	SqliteDB = db
+}
+
+func GetSqlitedb() *gorm.DB {
+	sqlDB, err := SqliteDB.DB()
+	if err != nil {
+		log.Print("connect db server failed.")
+		InitsqLite()
+	}
+	if err := sqlDB.Ping(); err != nil {
+		err := sqlDB.Close()
+		if err != nil {
+			return nil
+		}
+		InitsqLite()
+	}
+	return SqliteDB
+}
+
+type NewTable struct {
+	Table interface{}
+	Name  string
+}
+
+func SqliteCreateTable(Name string, Table interface{}) error {
+	err := GetSqlitedb().AutoMigrate(Table)
+	if err != nil {
+		fmt.Printf("sqlite创建 %s 表失败!!!", Name)
+	}
+	return err
+}
