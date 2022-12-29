bdb6797d80dab6a6e71bf68df76d14e622d62d04
xu756 <756334744@qq.com>
2022-12-29 23:48:49 +0800

    create logs struct

--- app/system/logic.go
+++ app/system/logic.go
@@ -3,8 +3,10 @@ package system
 import (
 	"context"
 	"github.com/cloudwego/hertz/pkg/app"
+	"server/common/log"
+	"server/common/model"
 )
 
 func Reset(ctx context.Context, c *app.RequestContext) {
-
+	model.SqliteCreateTable("logs", log.Logs{})
 }
