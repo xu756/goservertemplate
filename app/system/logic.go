442c6279172bd4fe59b3901019a4c4be64dc4c2e
xu756 <756334744@qq.com>
2022-12-30 23:41:44 +0800

    测试接口

--- app/system/logic.go
+++ app/system/logic.go
@@ -3,10 +3,16 @@ package system
 import (
 	"context"
 	"github.com/cloudwego/hertz/pkg/app"
-	"server/common/log"
+	"server/common/logs"
 	"server/common/model"
+	"server/common/response"
 )
 
 func Reset(ctx context.Context, c *app.RequestContext) {
-	model.SqliteCreateTable("logs", log.Logs{})
+	err := model.SqliteCreateTable(&logs.Logs{})
+	if err != nil {
+		response.ErrorRes(c, "创建log表错误", err)
+		return
+	}
+	response.SuccessRes(c, "重置成功", nil)
 }
