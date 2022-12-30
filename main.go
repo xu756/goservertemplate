3380937d97a4a29bbc34d7fe6eb97611c16acf86
xu756 <756334744@qq.com>
2022-12-30 23:42:50 +0800

    配置

--- main.go
+++ main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"server/common/config"
+	"server/common/model"
 	"server/router"
 )
 
 func main() {
 	config.Config()
+	model.InitsqLite()
+	model.InitMysqlDB()
 	router.InitRouter()
 }
